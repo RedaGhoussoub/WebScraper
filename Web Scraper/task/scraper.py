3459ae87fee285d7a1e41e3d1b8c500efe19c3e2
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2023-03-01 01:34:11 -0500

    Modified tag to be parsed

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -18,14 +18,14 @@ for page in range(1, number_of_pages + 1):
     response = requests.get(f'{NATURE_PREFIX_URL}/nature/articles?sort=PubDate&year=2020&page={page}')
     soup = BeautifulSoup(response.content, 'html.parser')
     links = [link.find_parent('article').find('a', {'data-track-action': 'view article'}).get('href')
-             for link in soup.find_all('span', {'class': 'c-meta__type'}, text=article_type)]
+             for link in soup.find_all('span', {'class': 'c-meta__type'}, string=article_type)]
 
     for link in links:
         response = requests.get(f'{NATURE_PREFIX_URL}{link}')
         soup = BeautifulSoup(response.content, 'html.parser')
         title = '_'.join(soup.find('title').get_text().translate(
             str.maketrans('', '', string.punctuation + '’')).strip().split())
-        body = soup.find('div', attrs={'class': 'c-article-body main-content'}).get_text().strip().encode('utf-8')
+        body = soup.find('p', attrs={'class': 'article__teaser'}).get_text().strip().encode('utf-8')
         with open(f'{title}.txt', 'wb') as file:
             file.write(body)
 
